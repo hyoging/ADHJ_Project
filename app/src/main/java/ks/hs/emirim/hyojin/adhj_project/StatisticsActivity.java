ed41433ca163fd5ee074586d378eec012eaaf0fa
jimin <w2037@e-mirim.hs.kr>
2021-09-14 19:33:37 +0900

    PieChart 생성

--- app/src/main/java/ks/hs/emirim/hyojin/adhj_project/StatisticsActivity.java
+++ app/src/main/java/ks/hs/emirim/hyojin/adhj_project/StatisticsActivity.java
@@ -3,11 +3,22 @@ package ks.hs.emirim.hyojin.adhj_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+
 public class StatisticsActivity extends AppCompatActivity {
+    PieChart pieChart;
+
+    public StatisticsActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,37 @@ public class StatisticsActivity extends AppCompatActivity {
 
         Button btn4 = findViewById(R.id.btn4);
         btn4.setOnClickListener(btnListener4);
+
+        pieChart = (PieChart)findViewById(R.id.piechart);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setHoleColor(Color.YELLOW);
+        pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
+
+        yValues.add(new PieEntry(34f,"식비"));
+        yValues.add(new PieEntry(23f,"교통비"));
+        yValues.add(new PieEntry(14f,"취미"));
+        yValues.add(new PieEntry(35f,"생필품"));
+        yValues.add(new PieEntry(40f,"뷰티"));
+        yValues.add(new PieEntry(40f,"신발"));
+        yValues.add(new PieEntry(5f,"옷"));
+        yValues.add(new PieEntry(2f,"야식"));
+
+        Description description = new Description();
+        description.setText("6월 통계");
+        description.setTextSize(35);
+        pieChart.setDescription(description);
+
+
+
     }
 
     View.OnClickListener btnListener1 = new View.OnClickListener() {
@@ -59,4 +101,5 @@ public class StatisticsActivity extends AppCompatActivity {
             finish();
         }
     };
+
 }
