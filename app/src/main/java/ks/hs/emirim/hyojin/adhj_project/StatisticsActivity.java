fd4cb5a08cbe7f1ce40421e62e571b52f03d79b5
jimin <w2037@e-mirim.hs.kr>
2021-09-23 18:58:00 +0900

    뒤로가기 버튼 추가

--- app/src/main/java/ks/hs/emirim/hyojin/adhj_project/StatisticsActivity.java
+++ app/src/main/java/ks/hs/emirim/hyojin/adhj_project/StatisticsActivity.java
@@ -1,13 +1,19 @@
 package ks.hs.emirim.hyojin.adhj_project;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
@@ -24,10 +30,17 @@ public class StatisticsActivity extends AppCompatActivity {
     public StatisticsActivity() {
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        Toolbar toolbar = findViewById(R.id.next_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//뒤로가기
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitleTextColor(Color.rgb(100, 98, 98));
 
         Button btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(btnListener1);
@@ -41,11 +54,11 @@ public class StatisticsActivity extends AppCompatActivity {
         Button btn4 = findViewById(R.id.btn4);
         btn4.setOnClickListener(btnListener4);
 
-        pieChart = (PieChart)findViewById(R.id.piechart);
+        pieChart = (PieChart) findViewById(R.id.piechart);
 
         pieChart.setUsePercentValues(true);
         pieChart.getDescription().setEnabled(false);
-        pieChart.setExtraOffsets(5,10,5,5);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
 
         pieChart.setDragDecelerationFrictionCoef(0.95f);
 
@@ -55,13 +68,13 @@ public class StatisticsActivity extends AppCompatActivity {
 
         ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
 
-        yValues.add(new PieEntry(34f,"식비"));
-        yValues.add(new PieEntry(23f,"교통비"));
-        yValues.add(new PieEntry(14f,"취미"));
-        yValues.add(new PieEntry(35f,"생필품"));
-        yValues.add(new PieEntry(40f,"뷰티"));
+        yValues.add(new PieEntry(34f, "식비"));
+        yValues.add(new PieEntry(23f, "교통비"));
+        yValues.add(new PieEntry(14f, "취미"));
+        yValues.add(new PieEntry(35f, "생필품"));
+        yValues.add(new PieEntry(40f, "뷰티"));
 
-        PieDataSet dataSet = new PieDataSet(yValues,"account book");
+        PieDataSet dataSet = new PieDataSet(yValues, "account book");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(35);
         dataSet.setValueTextSize(25);
@@ -78,7 +91,7 @@ public class StatisticsActivity extends AppCompatActivity {
     View.OnClickListener btnListener1 = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }
@@ -107,5 +120,15 @@ public class StatisticsActivity extends AppCompatActivity {
             finish();
         }
     };
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
