9c7c3f2fd69f9b146cb839159f97cd292904ea15
jimin <w2037@e-mirim.hs.kr>
2021-09-23 18:28:48 +0900

    PieChart 수정

--- app/src/main/java/ks/hs/emirim/hyojin/adhj_project/StatisticsActivity.java
+++ app/src/main/java/ks/hs/emirim/hyojin/adhj_project/StatisticsActivity.java
@@ -10,7 +10,10 @@ import android.widget.Button;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -56,16 +59,18 @@ public class StatisticsActivity extends AppCompatActivity {
         yValues.add(new PieEntry(14f,"취미"));
         yValues.add(new PieEntry(35f,"생필품"));
         yValues.add(new PieEntry(40f,"뷰티"));
-        yValues.add(new PieEntry(40f,"신발"));
-        yValues.add(new PieEntry(5f,"옷"));
-        yValues.add(new PieEntry(2f,"야식"));
 
-        Description description = new Description();
-        description.setText("6월 통계");
-        description.setTextSize(35);
-        pieChart.setDescription(description);
+        PieDataSet dataSet = new PieDataSet(yValues,"account book");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(35);
+        dataSet.setValueTextSize(25);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(25);
+        data.setValueTextColor(Color.YELLOW);
 
+        pieChart.setData(data);
 
     }
 
